3e2d4b037ec423448f8fd4b88b0015266b279670
JosineyJr <josineyjunior14@gmail.com>
2023-04-12 10:28:04 -0300

    refactor: 💡 add accounts module
    Generate accounts module using nest cli command

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonsModule } from './modules/persons/persons.module';
+import { AccountsModule } from './modules/accounts/accounts.module';
 
 @Module({
-  imports: [PersonsModule],
+  imports: [PersonsModule, AccountsModule],
   controllers: [AppController],
   providers: [AppService],
 })
